8eaa81cce60c89d3165f8dd6c0fe9a4bb6ac2478
Devesh Tuli <deveshtuli02@gmail.com>
2025-02-08 12:37:38 +0530

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -64,6 +64,7 @@ const projectsData=[{
 },{
     title:"Playora",
     image:"https://res.cloudinary.com/dqgrwjod2/image/upload/v1738994119/Screenshot_2025-02-08_at_11.24.40_AM_g1scoi.png",
+    desc:"Playora is a modern and responsive YouTube clone built using React.js and Material UI. It provides a seamless video streaming experience with an intuitive and aesthetically pleasing user interface.",
     skills:[{
         name:"React",
         svg:<FaReact/>
